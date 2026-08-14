c49430627cd8b137b0dd9c1a7d7e9b0688790548
Nolane-x <casioreview20@gmail.com>
2026-08-14 10:41:27 +0700

    feat(v9): export product completeness APIs

--- src/nolane_ui/__init__.py
+++ src/nolane_ui/__init__.py
@@ -19,14 +19,30 @@ from .validators import (
     validate_v6_completion_evidence,
     validate_v7_completion_evidence,
     validate_v8_completion_evidence,
+    validate_v9_completion_evidence,
 )
+from .product_v9 import (
+    validate_account_workspace_lifecycle,
+    validate_capability_envelope,
+    validate_domain_audience_fit,
+    validate_interface_residue_audit,
+    validate_render_critique,
+    validate_render_fidelity,
+    validate_settings_architecture,
+    validate_taste_comparison,
+    validate_v9_product_system,
+)
+from .scope_v9 import validate_scope_adequacy
 
 __all__ = [
     "validate_completion_packet", "validate_repository", "validate_skill_graph",
     "validate_state_matrix", "validate_tokens", "validate_industry_atlas",
     "validate_source_ledger", "validate_research_saturation", "validate_bounded_saturation",
     "validate_research_radar", "validate_mandatory_routes", "mandatory_routes_for_profile",
-    "validate_v3_completion_evidence", "validate_v4_completion_evidence", "validate_v5_completion_evidence", "validate_v6_completion_evidence", "validate_v7_completion_evidence", "validate_v8_completion_evidence",
+    "validate_v3_completion_evidence", "validate_v4_completion_evidence", "validate_v5_completion_evidence", "validate_v6_completion_evidence", "validate_v7_completion_evidence", "validate_v8_completion_evidence", "validate_v9_completion_evidence",
+    "validate_capability_envelope", "validate_scope_adequacy", "validate_settings_architecture",
+    "validate_account_workspace_lifecycle", "validate_interface_residue_audit", "validate_taste_comparison",
+    "validate_render_critique", "validate_domain_audience_fit", "validate_render_fidelity", "validate_v9_product_system",
     "query_ui_ecosystem", "validate_ui_ecosystem_registry", "validate_reference_ledger",
     "validate_source_selection", "validate_rich_interaction_contract", "validate_ui_integration_audit",
 ]
