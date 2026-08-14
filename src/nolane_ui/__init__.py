3696f717ae561ae1559525595025e8cee8767d73
Nolane-x <casioreview20@gmail.com>
2026-08-14 11:54:34 +0700

    feat(v10): export empirical proof kernels

--- src/nolane_ui/__init__.py
+++ src/nolane_ui/__init__.py
@@ -33,6 +33,13 @@ from .product_v9 import (
     validate_v9_product_system,
 )
 from .scope_v9 import validate_scope_adequacy
+from .behavior_v10 import validate_hypothesis_registry
+from .benchmark_v10 import validate_task_corpus, materialize_task_for_generation, materialize_task_for_judge
+from .mutation_v10 import validate_mutation_registry, expected_mutation_effects
+from .experiment_v10 import validate_experiment_manifest, validate_run_record, pairing_key
+from .judging_v10 import blind_run_for_judge, pair_orientation, detect_leakage, validate_judgment
+from .stats_v10 import paired_delta, bootstrap_ci, summarize_paired, evaluate_ablation_recovery, aggregate_dimension
+from .claims_v10 import promote_claim, validate_claim
 
 __all__ = [
     "validate_completion_packet", "validate_repository", "validate_skill_graph",
@@ -43,6 +50,11 @@ __all__ = [
     "validate_capability_envelope", "validate_scope_adequacy", "validate_settings_architecture",
     "validate_account_workspace_lifecycle", "validate_interface_residue_audit", "validate_taste_comparison",
     "validate_render_critique", "validate_domain_audience_fit", "validate_render_fidelity", "validate_v9_product_system",
+    "validate_hypothesis_registry", "validate_task_corpus", "materialize_task_for_generation", "materialize_task_for_judge",
+    "validate_mutation_registry", "expected_mutation_effects", "validate_experiment_manifest", "validate_run_record", "pairing_key",
+    "blind_run_for_judge", "pair_orientation", "detect_leakage", "validate_judgment",
+    "paired_delta", "bootstrap_ci", "summarize_paired", "evaluate_ablation_recovery", "aggregate_dimension",
+    "promote_claim", "validate_claim",
     "query_ui_ecosystem", "validate_ui_ecosystem_registry", "validate_reference_ledger",
     "validate_source_selection", "validate_rich_interaction_contract", "validate_ui_integration_audit",
 ]
