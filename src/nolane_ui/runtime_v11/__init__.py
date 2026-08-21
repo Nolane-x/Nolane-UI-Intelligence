8ef950e5a7d316a3cf4a0fb4ca4af99adb95c958
Nolane-x <casioreview20@gmail.com>
2026-08-21 20:11:47 +0700

    V11 Phase 5: export live visual runtime APIs

--- src/nolane_ui/runtime_v11/__init__.py
+++ src/nolane_ui/runtime_v11/__init__.py
@@ -8,6 +8,11 @@ from .browser import (
     normalize_browser_observation,
     validate_browser_observation,
 )
+from .browser_transport import (
+    build_browser_transport_capability,
+    require_transport_capabilities,
+    validate_browser_transport_capability,
+)
 from .design_memory import assess_design_memory_staleness, build_design_memory, validate_design_memory
 from .detector import scan_path, scan_text
 from .doctor import REQUIRED_RUNTIME_ARTIFACTS, diagnose_runtime_state
@@ -26,14 +31,37 @@ from .live import (
     transactional_replace,
     validate_live_session,
 )
+from .live_visual import (
+    accept_live_visual_preview,
+    assess_visual_observation_capabilities,
+    prepare_live_visual_preview,
+    prepare_live_visual_selection,
+)
+from .overlay import build_overlay_packet, validate_overlay_packet
+from .playwright_adapter import (
+    collect_playwright_observation,
+    inject_playwright_preview,
+    playwright_available,
+    playwright_capability,
+    refresh_playwright_preview,
+)
+from .preview import (
+    assess_preview_freshness,
+    build_preview_candidate,
+    prepare_preview_application,
+    record_preview_observation,
+    validate_preview_candidate,
+)
 from .quality_residue import assess_quality_residue_closure, plan_quality_residue_pass
 from .registry import load_rule_registry, validate_rule_registry
 from .reobserve import compare_runtime_observations
 from .routing import route_runtime_finding, route_runtime_findings
+from .source_attribution import resolve_source_attribution, select_source_candidate, validate_source_attribution
 from .taste_court import aggregate_taste_court, prepare_blinded_candidates, validate_taste_judgment
 
 __all__ = [
     "REQUIRED_RUNTIME_ARTIFACTS",
+    "accept_live_visual_preview",
     "adjudicate_findings",
     "adjudicate_match",
     "aggregate_taste_court",
@@ -41,35 +69,56 @@ __all__ = [
     "assess_design_memory_staleness",
     "assess_evidence_staleness",
     "assess_genericity",
+    "assess_preview_freshness",
     "assess_quality_residue_closure",
+    "assess_visual_observation_capabilities",
     "browser_observation_findings",
+    "build_browser_transport_capability",
     "build_design_memory",
     "build_evidence_binding",
     "build_hook_capability",
+    "build_overlay_packet",
+    "build_preview_candidate",
+    "collect_playwright_observation",
     "commit_direction",
     "compare_runtime_observations",
     "compile_aesthetic_intent",
     "create_live_session",
     "diagnose_runtime_state",
     "evaluate_direction_candidates",
+    "inject_playwright_preview",
     "load_rule_registry",
     "normalize_browser_observation",
     "plan_quality_residue_pass",
+    "playwright_available",
+    "playwright_capability",
     "prepare_blinded_candidates",
+    "prepare_live_visual_preview",
+    "prepare_live_visual_selection",
+    "prepare_preview_application",
     "product_substitution_assessment",
+    "record_preview_observation",
+    "refresh_playwright_preview",
+    "require_transport_capabilities",
+    "resolve_source_attribution",
     "route_runtime_finding",
     "route_runtime_findings",
     "scan_path",
     "scan_text",
+    "select_source_candidate",
     "sha256_file",
     "sha256_text",
     "transactional_replace",
     "validate_aesthetic_intent",
     "validate_browser_observation",
+    "validate_browser_transport_capability",
     "validate_design_memory",
     "validate_evidence_binding",
     "validate_live_session",
+    "validate_overlay_packet",
+    "validate_preview_candidate",
     "validate_rule_registry",
+    "validate_source_attribution",
     "validate_taste_judgment",
     "validate_trend_registry",
 ]
